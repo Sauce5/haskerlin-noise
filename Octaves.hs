f1ed919631ea330ebeec414b72b316a34bd1c8a7
Sauce5 <alastair.sauce@gmail.com>
2023-12-07 15:41:58 -0500

    Moved octave generation code into wrapper function

--- Octaves.hs
+++ Octaves.hs
@@ -1,13 +1,44 @@
 module Octaves
-( octave
+( genOctave
 ) where
 
 import Data.List (transpose)
+import System.Random (mkStdGen, Random(randomR), StdGen)
 
 import NoiseTypes
 import MapInit
 import Gradients
 import Smoothing
+
+{-
+    Generate an octave from a seed
+    Seed -> Chunk Length -> Chunk Size -> Map
+-}
+genOctave :: Int -> Integer -> Integer -> Map
+genOctave seed ln sz = do
+    -- default parameters for now
+    let chunks_length = ln       -- map is chunks_length x chunks_length
+    let chunk_size = sz          -- each chunk is chunk_size x chunk_size
+    -- number of vectors to make
+    let num_vectors = nextSquare chunks_length
+    -- generate randomly rotated vectors
+    let gen = mkStdGen seed
+    let rands = nextN (3*num_vectors) gen           -- each pair of nums makes a Vector
+    let infl_vect_list = randomsToVectors rands
+    -- init map and corner map
+    let corner_map = vectors infl_vect_list (chunks_length + 1)
+    octave corner_map chunks_length chunk_size
+
+nextSquare :: Integer -> Integer
+nextSquare n = (n + 1) * (n + 1)
+
+nextN :: Integer -> StdGen -> [Integer]
+nextN n gen = case abs n of
+    0 -> []
+    _ -> do
+        let rn = randomR (0::Integer,9::Integer) gen
+        let nx = fst rn
+        nx : nextN (abs n - 1) (snd rn)
 
 {-
     Generates an octave of Perlin noise.
--- terrain-gen.hs
+++ terrain-gen.hs
@@ -2,43 +2,20 @@ import System.Random (mkStdGen, Random(randomR), StdGen)
 import Data.Time.Clock.System (getSystemTime, systemNanoseconds)
 import Data.List (transpose)
 
-import NoiseTypes
-import MapInit
-import Gradients
 import Display
-import Smoothing
 import Octaves
-
-nextN :: Integer -> StdGen -> [Integer]
-nextN n gen = case abs n of
-    0 -> []
-    _ -> do
-        let rn = randomR (0::Integer,9::Integer) gen
-        let nx = fst rn
-        nx : nextN (abs n - 1) (snd rn)
-
-nextSquare :: Integer -> Integer
-nextSquare n = (n + 1) * (n + 1)
 
 -- main function
 main :: IO ()
 main = do
     putStr "Seed: "
-    seed <- getLine
-    -- default parameters for now
-    let chunks_length = 5       -- map is chunks_length x chunks_length
-    let chunk_size = 16          -- each chunk is chunk_size x chunk_size
-    -- number of vectors to make
-    let num_vectors = nextSquare chunks_length
-    -- generate randomly rotated vectors
-    let gen = mkStdGen (read seed)
-    let rands = nextN (3*num_vectors) gen           -- each pair of nums makes a Vector
-    let infl_vect_list = randomsToVectors rands
-    -- init map and corner map
-    let corner_map = vectors infl_vect_list (chunks_length + 1)
-
+    inpSeed <- getLine
+    {-
+        Error checking on the seed will go here.
+    -}
+    let seed = read inpSeed
     -- generate one octave of Perlin noise
-    let octave1 = octave corner_map chunks_length chunk_size
+    let octave1 = genOctave seed 5 16
     -- print octave
     putStrLn $ stringMap octave1
     
